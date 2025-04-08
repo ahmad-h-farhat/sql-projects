bfd98dfdb9c32405b11f2b9921d4f6e9da1c3d82
Ahmad Farhat <ahmadd1812@hotmail.com>
2025-04-08 09:12:30 +0200

    git add etl_finance/portfolio_breakdown.sql git commit -m "Add KPI for portfolio breakdown by asset type"

---
+++ etl_finance/portfolio_breakdown.sql
@@ -0,0 +1,10 @@
+-- portfolio_breakdown.sql
+-- KPI: Portfolio breakdown by asset type in USD
+
+SELECT
+    asset_type,
+    ROUND(SUM(amount_usd), 2) AS total_investment_usd,
+    ROUND(SUM(amount_usd) * 100.0 / SUM(SUM(amount_usd)) OVER (), 2) AS percentage_of_portfolio
+FROM cleaned_transactions
+GROUP BY asset_type
+ORDER BY total_investment_usd DESC;
