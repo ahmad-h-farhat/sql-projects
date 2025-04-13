37a22057e8efe68c9418f50361eda0f547f16601
Ahmad Farhat <ahmadd1812@hotmail.com>
2025-04-13 08:05:11 +0200

    Update portfolio_breakdown.sql query

--- etl_finance/kpi_analytics/portfolio_breakdown.sql
+++ etl_finance/kpi_analytics/portfolio_breakdown.sql
@@ -5,6 +5,6 @@ SELECT
     asset_type,
     ROUND(SUM(amount_usd), 2) AS total_investment_usd,
     ROUND(SUM(amount_usd) * 100.0 / SUM(SUM(amount_usd)) OVER (), 2) AS percentage_of_portfolio
-FROM cleaned_transactions
+FROM clean_transactions
 GROUP BY asset_type
 ORDER BY total_investment_usd DESC;
